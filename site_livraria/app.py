4fe10bdf18e1984d3fd54da796ee83b5b180b178
ciliadeiros <ceciliamed21@gmail.com>
2025-07-11 17:24:56 -0300

    atualizei as rotas :)

--- site_livraria/app.py
+++ site_livraria/app.py
@@ -1,26 +1,43 @@
-from flask import Flask, render_template #, request, redirect, url_for, session, make_response, flash
-# from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
+from flask import Flask, render_template , request, redirect, url_for, session, make_response, flash
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
+import sqlite3
 #cria o app
 app = Flask(__name__)
 
+def obter_conexao():
+    ''' Cria uma conexão com o arquivo de banco de dados banco.db.
+        Se o arquivo não existir, ele será criado automaticamente. '''
+    conexao = sqlite3.connect('banco.db')
+    conexao.row_factory = sqlite3.Row
+    return conexao
+
+@login_required
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/cadastro')
-def index():
+@app.route('/cadastro', methods=['POST', 'GET'])
+def cadastro():
+    if request.method == 'POST':
+        email = request.form['email']
+        nome = request.form['nome']
+        senha = request.form['senha']
+        preferencias = request.form['preferencias']
+
     return render_template('cadastro.html')
 
 @app.route('/login')
-def index():
+def login():
     return render_template('login.html')
 
+@login_required
 @app.route('/livros')
-def index():
+def livros():
     return render_template('livros.html')
 
+@login_required
 @app.route('/biblioteca')
-def index():
+def biblioteca():
     return render_template('biblioteca.html')
 
 # Rodar o app
